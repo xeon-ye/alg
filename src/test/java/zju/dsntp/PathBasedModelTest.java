42ea60020b4884f4a0d2c503ea61b4ff8b4fe69d
xuchengsi <924094856@qq.com>
2016-09-19 17:15:16 +0800

    补全了路径搜索的程序和测试

--- src/test/java/zju/dsntp/PathBasedModelTest.java
+++ src/test/java/zju/dsntp/PathBasedModelTest.java
@@ -26,6 +26,45 @@ public class PathBasedModelTest extends TestCase implements DsModelCons {
     public void testDscase13() {
         PathBasedModel model = new PathBasedModel(IeeeDsInHand.FEEDER13);
         model.buildPathes();
-        assertEquals(10, model.getPathes().size());//
+        assertEquals(12, model.getPathes().size());
+    }
+
+    public void testDscase34() {
+        PathBasedModel model = new PathBasedModel(IeeeDsInHand.FEEDER34);
+        model.buildPathes();
+        assertEquals(33, model.getPathes().size());
+    }
+
+    public void testDscase37() {
+        PathBasedModel model = new PathBasedModel(IeeeDsInHand.FEEDER37);
+        model.buildPathes();
+        assertEquals(36, model.getPathes().size());
+    }
+
+    public void testDscase4() {
+        PathBasedModel model = new PathBasedModel(IeeeDsInHand.FEEDER4_DD_B);
+        model.buildPathes();
+        assertEquals(3, model.getPathes().size());
+        model = new PathBasedModel(IeeeDsInHand.FEEDER4_DD_UNB);
+        model.buildPathes();
+        assertEquals(3, model.getPathes().size());
+        model = new PathBasedModel(IeeeDsInHand.FEEDER4_DGrY_B);
+        model.buildPathes();
+        assertEquals(3, model.getPathes().size());
+        model = new PathBasedModel(IeeeDsInHand.FEEDER4_DGrY_UNB);
+        model.buildPathes();
+        assertEquals(3, model.getPathes().size());
+        model = new PathBasedModel(IeeeDsInHand.FEEDER4_GrYGrY_B);
+        model.buildPathes();
+        assertEquals(3, model.getPathes().size());
+        model = new PathBasedModel(IeeeDsInHand.FEEDER4_GrYGrY_UNB);
+        model.buildPathes();
+        assertEquals(3, model.getPathes().size());
+    }
+
+    public void testDscase123() {
+        PathBasedModel model = new PathBasedModel(IeeeDsInHand.FEEDER123);
+        model.buildPathes();
+        assertEquals(193, model.getPathes().size());
     }
 }
