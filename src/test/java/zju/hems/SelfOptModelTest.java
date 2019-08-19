cceae36a729cf698286988eb7d9a1cc00707c072
xuchengsi <924094856@qq.com>
2019-08-19 20:00:26 +0800

    修正需求响应成本计算程序

--- src/test/java/zju/hems/SelfOptModelTest.java
+++ src/test/java/zju/hems/SelfOptModelTest.java
@@ -17,7 +17,7 @@ public class SelfOptModelTest  extends TestCase {
     double[] pvPowers = new double[periodNum];   // 光伏出力
     double[] Lsteams = new double[periodNum];   // 蒸汽负荷
 
-    public void testSelfOpt() throws IOException {
+    public SelfOptModel selfOptTestModel() throws IOException {
         InputStream inputStream = this.getClass().getResourceAsStream("/iesfiles/selfopt/input_with_cold.csv");
         readData(inputStream);
         List<AbsorptionChiller> absorptionChillers = new ArrayList<>(1);
@@ -56,11 +56,61 @@ public class SelfOptModelTest  extends TestCase {
         Microgrid microgrid = new Microgrid(users);
         Map<String, double[]> gatePowers = new HashMap<>();   // 用户关口功率
         double[] gatePower = new double[periodNum];
-        for (int i = 0; i < gatePower.length; i++) {
+        for (int i = 0; i < periodNum; i++) {
             gatePower[i] = 8000;
         }
         gatePowers.put(user.getUserId(), gatePower);
-        SelfOptModel selfOptModel = new SelfOptModel(microgrid, periodNum, acLoad, dcLoad, coolingLoad, elecPrices, gasPrices, steamPrices, gatePowers);
+        return new SelfOptModel(microgrid, periodNum, acLoad, dcLoad, coolingLoad, elecPrices, gasPrices, steamPrices, gatePowers);
+    }
+
+    public DemandRespModel demandRespTestModel() throws IOException {
+        InputStream inputStream = this.getClass().getResourceAsStream("/iesfiles/selfopt/input_with_cold.csv");
+        readData(inputStream);
+        List<AbsorptionChiller> absorptionChillers = new ArrayList<>(1);
+        AbsorptionChiller absorptionChiller = new AbsorptionChiller(0.00008, 0, 1000, 0.8);
+        absorptionChillers.add(absorptionChiller);
+        List<AirCon> airCons = new ArrayList<>(1);
+        AirCon airCon = new AirCon(0.0097, 1, 1.05, 0, 500, 4.3);
+        airCons.add(airCon);
+        List<Converter> converters = new ArrayList<>(1);
+        Converter converter = new Converter(0.95, 0.95);
+        converters.add(converter);
+        List<GasBoiler> gasBoilers = new ArrayList<>(1);
+        GasBoiler gasBoiler = new GasBoiler(0.04, 100, 0.85, 0, 1000, 500, 0);
+        gasBoilers.add(gasBoiler);
+        List<GasTurbine> gasTurbines = new ArrayList<>(1);
+        GasTurbine gasTurbine = new GasTurbine(0.063, 0.33, 0.6, 200, 50, 1000, - 500, 500, 0);
+        gasTurbines.add(gasTurbine);
+        List<IceStorageAc> iceStorageAcs = new ArrayList<>(1);
+        IceStorageAc iceStorageAc = new IceStorageAc(0.01, 1, 3, 3, 0.9, 1,
+                0.002, 500, 3000, 0.1, 0.95, 0.1, 1.05, 500, 500);
+        iceStorageAcs.add(iceStorageAc);
+        List<Photovoltaic> photovoltaics = new ArrayList<>(1);
+        Photovoltaic photovoltaic = new Photovoltaic(0.0005, pvPowers);
+        photovoltaics.add(photovoltaic);
+        List<SteamLoad> steamLoads = new ArrayList<>(1);
+        SteamLoad steamLoad = new SteamLoad(Lsteams, 0.8);
+        steamLoads.add(steamLoad);
+        List<Storage> storages = new ArrayList<>(3);
+        for (int i = 0; i < 2; i++) {
+            Storage storage = new Storage(0.005, 0.00075, 1250, 1250, 13000, 0.1, 0.9, 0.1, 0.5, 0.5, 0.0025, 0.95, 0.95);
+            storages.add(storage);
+        }
+        User user = new User("1", absorptionChillers, airCons, converters, gasBoilers, gasTurbines, iceStorageAcs, photovoltaics, steamLoads, storages);
+        Map<String, User> users = new HashMap<>();
+        users.put(user.getUserId(), user);
+        Microgrid microgrid = new Microgrid(users);
+        Map<String, double[]> gatePowers = new HashMap<>();   // 用户关口功率
+        double[] gatePower = new double[periodNum];
+        for (int i = 0; i < periodNum; i++) {
+            gatePower[i] = 8000;
+        }
+        gatePowers.put(user.getUserId(), gatePower);
+        return new DemandRespModel(microgrid, periodNum, acLoad, dcLoad, coolingLoad, elecPrices, gasPrices, steamPrices, gatePowers);
+    }
+
+    public void testSelfOpt() throws IOException {
+        SelfOptModel selfOptModel = selfOptTestModel();
         selfOptModel.mgSelfOpt();
         Map<String, UserResult> microgridResult = selfOptModel.getMicrogridResult();
         for (UserResult userResult : microgridResult.values()) {
@@ -73,75 +123,46 @@ public class SelfOptModelTest  extends TestCase {
     }
 
     public void testDemandResp() throws IOException {
-        InputStream inputStream = this.getClass().getResourceAsStream("/iesfiles/selfopt/input_with_cold.csv");
-        readData(inputStream);
-        List<AbsorptionChiller> absorptionChillers = new ArrayList<>(1);
-        AbsorptionChiller absorptionChiller = new AbsorptionChiller(0.00008, 0, 1000, 0.8);
-        absorptionChillers.add(absorptionChiller);
-        List<AirCon> airCons = new ArrayList<>(1);
-        AirCon airCon = new AirCon(0.0097, 1, 1.05, 0, 500, 4.3);
-        airCons.add(airCon);
-        List<Converter> converters = new ArrayList<>(1);
-        Converter converter = new Converter(0.95, 0.95);
-        converters.add(converter);
-        List<GasBoiler> gasBoilers = new ArrayList<>(1);
-        GasBoiler gasBoiler = new GasBoiler(0.04, 100, 0.85, 0, 1000, 500, 0);
-        gasBoilers.add(gasBoiler);
-        List<GasTurbine> gasTurbines = new ArrayList<>(1);
-        GasTurbine gasTurbine = new GasTurbine(0.063, 0.33, 0.6, 200, 50, 1000, - 500, 500, 0);
-        gasTurbines.add(gasTurbine);
-        List<IceStorageAc> iceStorageAcs = new ArrayList<>(1);
-        IceStorageAc iceStorageAc = new IceStorageAc(0.01, 1, 3, 3, 0.9, 1,
-                0.002, 500, 3000, 0.1, 0.95, 0.1, 1.05, 500, 500);
-        iceStorageAcs.add(iceStorageAc);
-        List<Photovoltaic> photovoltaics = new ArrayList<>(1);
-        Photovoltaic photovoltaic = new Photovoltaic(0.0005, pvPowers);
-        photovoltaics.add(photovoltaic);
-        List<SteamLoad> steamLoads = new ArrayList<>(1);
-        SteamLoad steamLoad = new SteamLoad(Lsteams, 0.8);
-        steamLoads.add(steamLoad);
-        List<Storage> storages = new ArrayList<>(3);
-        for (int i = 0; i < 2; i++) {
-            Storage storage = new Storage(0.005, 0.00075, 1250, 1250, 13000, 0.1, 0.9, 0.1, 0.5, 0.5, 0.0025, 0.95, 0.95);
-            storages.add(storage);
-        }
-        User user = new User("1", absorptionChillers, airCons, converters, gasBoilers, gasTurbines, iceStorageAcs, photovoltaics, steamLoads, storages);
-        Map<String, User> users = new HashMap<>();
-        users.put(user.getUserId(), user);
-        Microgrid microgrid = new Microgrid(users);
-        Map<String, double[]> gatePowers = new HashMap<>();   // 用户关口功率
-        double[] gatePower = new double[periodNum];
-        for (int i = 0; i < periodNum; i++) {
-            gatePower[i] = 8000;
-        }
-        gatePowers.put(user.getUserId(), gatePower);
-        SelfOptModel selfOptModel = new SelfOptModel(microgrid, periodNum, acLoad, dcLoad, coolingLoad, elecPrices, gasPrices, steamPrices, gatePowers);
-        selfOptModel.mgSelfOpt();
-        Map<String, UserResult> selfOptResult = selfOptModel.getMicrogridResult();
+        DemandRespModel demandRespModel = demandRespTestModel();
+        demandRespModel.mgSelfOpt();
+        Map<String, UserResult> selfOptResult = demandRespModel.getMicrogridResult();
         System.out.println(selfOptResult.get("1").getMinCost());
         System.out.println("---------自趋优计算结束---------");
 
+        Map<String, User> users = demandRespModel.getMicrogrid().getUsers();
+        // 原始关口功率
+        Map<String, double[]> origGatePowers = new HashMap<>();
+        for (String userId : users.keySet()) {
+            double[] ogGatePower = new double[periodNum];
+            for (int i = 0; i < periodNum; i++) {
+                ogGatePower[i] = demandRespModel.getGatePowers().get(userId)[i];
+            }
+            origGatePowers.put(userId, ogGatePower);
+        }
         // 关口功率指令
         int[] peakShaveTime = new int[periodNum];
         for (int i = 72; i < 76; i++) {
             peakShaveTime[i] = 1;
         }
-        selfOptModel.setPeakShaveTime(peakShaveTime);
+        demandRespModel.setPeakShaveTime(peakShaveTime);
         Map<String, double[]> insGatePowers = new HashMap<>();
-        double[] insGatePower = new double[periodNum];
-        for (int i = 0; i < periodNum; i++) {
-            if (peakShaveTime[i] == 1) {
-                insGatePower[i] = 3000;
-            } else {
-                insGatePower[i] = gatePower[i];
+        for (String userId : users.keySet()) {
+            double[] insGatePower = new double[periodNum];
+            for (int i = 0; i < periodNum; i++) {
+                if (peakShaveTime[i] == 1) {
+                    insGatePower[i] = 3000;
+                } else {
+                    insGatePower[i] = origGatePowers.get(userId)[i];
+                }
             }
+            insGatePowers.put(userId, insGatePower);
         }
-        insGatePowers.put(user.getUserId(), insGatePower);
-        selfOptModel.setInsGatePowers(insGatePowers);
+        demandRespModel.setInsGatePowers(insGatePowers);
         // 采样点数
-        int sampleNum = 19;
+        int sampleNum = 20;
         // 采样范围
-        double sampleRange = 1.9;
+        double sampleStart = 1.65;
+        double sampleEnd = 1.66;
         Map<String, double[]> increCosts = new HashMap<>(users.size());
         // 应削峰量
         Map<String, double[]> peakShavePowers = new HashMap<>(users.size());
@@ -151,18 +172,9 @@ public class SelfOptModelTest  extends TestCase {
             double[] purP = selfOptResult.get(userId).getPurP();
             for (int i = 0; i < periodNum; i++) {
                 if (peakShaveTime[i] == 1) {
-                    peakShavePowers.get(userId)[i] = purP[i] - selfOptModel.getInsGatePowers().get(userId)[i];
+                    peakShavePowers.get(userId)[i] = purP[i] - demandRespModel.getInsGatePowers().get(userId)[i];
                 }
             }
-        }
-        // 原始关口功率
-        Map<String, double[]> ogGatePowers = new HashMap<>();
-        for (String userId : users.keySet()) {
-            double[] ogGatePower = new double[periodNum];
-            for (int i = 0; i < periodNum; i++) {
-                ogGatePower[i] = selfOptModel.getGatePowers().get(userId)[i];
-            }
-            ogGatePowers.put(userId, ogGatePower);
         }
         for (int i = 0; i < sampleNum; i++) {
             Map<String, double[]> newGatePowers = new HashMap<>();
@@ -171,16 +183,16 @@ public class SelfOptModelTest  extends TestCase {
                 double[] newGatePower = new double[periodNum];
                 for (int j = 0; j < periodNum; j++) {
                     if (peakShaveTime[j] == 1) {
-                        newGatePower[j] = purP[j] - peakShavePowers.get(userId)[j] * sampleRange * (i + 1) / sampleNum;
+                        newGatePower[j] = purP[j] - peakShavePowers.get(userId)[j] * (sampleStart + (sampleEnd - sampleStart) * (i + 1) / sampleNum);
                     } else {
-                        newGatePower[j] = ogGatePowers.get(userId)[j];
+                        newGatePower[j] = origGatePowers.get(userId)[j];
                     }
                 }
                 newGatePowers.put(userId, newGatePower);
             }
-            selfOptModel.setGatePowers(newGatePowers);
-            selfOptModel.mgDemandResp();
-            Map<String, UserResult> microgridResult = selfOptModel.getMicrogridResult();
+            demandRespModel.setGatePowers(newGatePowers);
+            demandRespModel.mgDemandResp();
+            Map<String, UserResult> microgridResult = demandRespModel.getMicrogridResult();
             for (String userId : microgridResult.keySet()) {
                 UserResult userResult = microgridResult.get(userId);
                 System.out.println(userResult.getUserId() + "\t" + userResult.getStatus());
@@ -194,7 +206,7 @@ public class SelfOptModelTest  extends TestCase {
         for (String userId : users.keySet()) {
             double[] increCost = increCosts.get(userId);
             for (int i = 0; i < sampleNum; i++) {
-                System.out.println(sampleRange * (i + 1) / sampleNum + "," + increCost[i]);
+                System.out.println((sampleStart + (sampleEnd - sampleStart) * (i + 1) / sampleNum) + "," + increCost[i]);
             }
         }
     }
